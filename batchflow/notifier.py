57519d9324619c02d09574e037c056c013a6167e
alexeykozhevin <a.kozhevin@analysiscenter.org>
2019-05-27 17:49:52 +0300

    Fix pylint

--- batchflow/notifier.py
+++ batchflow/notifier.py
@@ -1,13 +1,15 @@
+""" Notifier """
+
 from abc import ABCMeta, abstractmethod
 import tqdm
 
 class Notifier(metaclass=ABCMeta):
     """ Abstract class for notifiers. """
-    
+
     @abstractmethod
     def close(self, *args, **kwargs):
         pass
-    
+
     @abstractmethod
     def update(self, *args, **kwargs):
         pass
@@ -19,12 +21,12 @@ class TqdmNotebookNotifier(Notifier):
     """ Notifier wrapper for tqdm.tqdm_notebook. """
     def __init__(self, *args, **kwargs):
         self._notifier = tqdm.tqdm_notebook(*args, **kwargs)
-    
+
     def close(self):
         self._notifier.close()
-    
+
     def update(self, n):
         self._notifier.update(n)
-    
+
     def __iter__(self):
         return iter(self._notifier)
